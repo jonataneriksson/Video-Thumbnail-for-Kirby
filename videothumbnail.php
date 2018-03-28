745288cbf3c05db2665300ea75c5b1cd5625043e
Jonatan Eriksson <joantan.eriksson@gmail.com>
2018-03-28 11:46:37 -0400

    Did not work.

--- videothumbnail.php
+++ videothumbnail.php
@@ -1,15 +1,5 @@
 <?php
 
-namespace Kirby\Component;
-
-require_once(__DIR__.DS.'lib'.DS.'thumb.php');
-require_once(__DIR__.DS.'lib'.DS.'component.php');
-require_once(__DIR__.DS.'lib'.DS.'drivers.php');
-
-// Initialize the plugin
-
-$kirby->set('component', 'thumb', 'Kirby\Component\VideoThumb');
-
 class VideothumbnailField extends InputListField {
 
   static public $assets = array(
@@ -171,3 +161,13 @@ class VideothumbnailField extends InputListField {
   }
 
 }
+
+namespace Kirby\Component;
+
+require_once(__DIR__.DS.'lib'.DS.'thumb.php');
+require_once(__DIR__.DS.'lib'.DS.'component.php');
+require_once(__DIR__.DS.'lib'.DS.'drivers.php');
+
+// Initialize the plugin
+
+$kirby->set('component', 'thumb', 'Kirby\Component\VideoThumb');
